2713fc5de834c600a0b4620e144a5e91aedebb56
mintesnotderib <mentesnot.derib@gmail.com>
2025-02-16 18:48:30 +0900

    this my first commit in guit hyb

---
+++ app.js
@@ -0,0 +1,17 @@
+const x=2;
+
+
+
+/*
+git status
+git init
+git commit -m "this is my commit"
+git push -u origin main
+git add origin https://github.com/mintesnotderib/personal-profile.git
+git push origin main
+git branch
+git checkout -b mybranch
+git add .
+git clone https://github.com/mintesnotderib/personal-profile.git
+*/
+
